7a87467e23b1bd2a7ff41c333a5b3ce9ba43201f
gcauvet <gcauvet@smaha-consulting.com>
2020-12-31 00:44:28 +0100

    destination service

--- src/services/destination.services.js
+++ src/services/destination.services.js
@@ -1,21 +1,34 @@
 import axios from 'axios';
+
 import { getRandomNumber } from '../helpers/getRandom';
-import parseDestination from '../helpers/destinationParser';
+import { parseDestinationFromAPI } from '../helpers/destinationParser';
+import parseImage from '../helpers/imageParser';
+
+const getFlag = (countryCode) =>
+    axios
+        .get(`https://restcountries.eu/rest/v2/alpha/${countryCode}?fields=flag`)
+        .then((flag) => flag)
+        .catch((err) => console.log(err));
+
+const getImage = async (country, countryCode) =>
+    axios
+        .get(
+            `https://api.unsplash.com/search/photos?page=1
+            &per_page=1
+            &query=${country}
+            &client_id=kHivcP5EXrCVXpgCYCP41tAcydkQozCyz__1CQiLG-s`
+        )
+        .then((image) => parseImage(image.data.results))
+        .catch(() =>
+            getFlag(countryCode)
+                .then((response) => response.data.flag)
+                .catch((err) => console.log(err))
+        );
 
 const getDestinationsFromAPI = () =>
     axios
-        .get(`https://random-data-api.com/api/address/random_address?size=${getRandomNumber(20, 10)}`)
-        .then((response) => response.data.map((data) => parseDestination(data)))
-        .catch((err) => {
-            throw new Error(err);
-        });
-
-const getImage = (query) =>
-    axios
-        .get(`https://api.unsplash.com/search/photos/?page=1&per_page=1&query=${query}&orientation=landscape&client_id=kHivcP5EXrCVXpgCYCP41tAcydkQozCyz__1CQiLG-s`)
-        .then((data) => data.data.results[0].urls.small)
-        .catch((err) => {
-            throw new Error(err);
-        });
+        .get(`https://random-data-api.com/api/address/random_address?size=${getRandomNumber(15, 10)}`)
+        .then((response) => response.data.map((data) => parseDestinationFromAPI(data)))
+        .catch((err) => console.log(err));
 
 export { getDestinationsFromAPI, getImage };
